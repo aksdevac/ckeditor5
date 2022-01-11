93dc184d093d0315935a3db528238dc963120165
Akash P <aksdevac@gmail.com>
2022-01-11 18:09:52 +0530

    add html embed & source-editing plugin

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -30,6 +30,8 @@ import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+import HtmlEmbed from '@ckeditor/ckeditor5-html-embed/src/htmlembed';
+import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -58,7 +60,9 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	HtmlEmbed,
+	SourceEditing
 ];
 
 // Editor configuration.
@@ -79,6 +83,8 @@ ClassicEditor.defaultConfig = {
 			'uploadImage',
 			'blockQuote',
 			'insertTable',
+			'sourceEditing',
+			'htmlEmbed',
 			'mediaEmbed',
 			'undo',
 			'redo'
